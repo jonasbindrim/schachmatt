8b877dee0ee4e1306abf5b1187024462666f3fb4
Jonas Bindrim <jonas.bindrim@hotmail.de>
2025-03-23 08:51:06 +0100

    Add example

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,20 +1,9 @@
-# Schachmatt
-
-Schachmatt is a chess library for rust
-
-## Intention
-
-This library can be used to run chess games, generate legal moves, create and work with standardized chess data formats.
-
-## Examples
-
-The following example starts a game of chess and plays random moves until the game is over
-
-```rust
 use schachmatt::{Game, GameResult};
 use rand::seq::IndexedRandom;
 use rand::rng;
 
+/// Starts a game of chess and plays random moves until the game is over.
+/// Afterwards the game result is printed.
 fn main() {
 
     let mut game = Game::default();
@@ -38,15 +27,3 @@ fn main() {
 
     println!("{}", game_result);
 }
-```
-
-Examples are available under `examples/`
-
-## Data format support
-
-Schachmatt can import and export chess game, position and moves in the following formats:
-
-- Forsyth-Edwards Notation (FEN)
-- Long algebraic notation (LAN)
-- Standard algebraic notation (SAN)
-- Portable game notation (PGN)
