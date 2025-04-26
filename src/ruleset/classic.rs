48e6624822f382bb132670aeda12ed32edb46cae
Jonas Bindrim <jonas.bindrim@hotmail.de>
2025-04-26 13:13:56 +0200

    (WIP) Move get_possible_moves into ruleset

--- src/ruleset/classic.rs
+++ src/ruleset/classic.rs
@@ -1,9 +1,16 @@
-use crate::Position;
+use crate::{Position, Turn};
 
 use super::Ruleset;
 
-pub const CLASSIC_RULESET: Ruleset = Ruleset::new(generate_initial_position);
+pub const CLASSIC_RULESET: Ruleset = Ruleset::new(
+    generate_initial_position,
+    get_possible_moves
+);
 
 fn generate_initial_position() -> Position {
     Position::default()
+}
+
+fn get_possible_moves(position: &Position) -> Vec<Turn> {
+    todo!()
 }
--- src/ruleset/mod.rs
+++ src/ruleset/mod.rs
@@ -1,4 +1,4 @@
-use crate::Position;
+use crate::{Position, Turn};
 
 pub(crate) mod classic;
 
@@ -6,16 +6,25 @@ pub(crate) mod classic;
 #[derive(Clone)]
 pub struct Ruleset {
     generate_initial_position: fn() -> Position,
+    get_possible_moves: fn(&Position) -> Vec<Turn>,
 }
 
 impl Ruleset {
-    pub const fn new(generate_initial_position: fn() -> Position) -> Self {
+    pub const fn new(
+        generate_initial_position: fn() -> Position,
+        get_possible_moves: fn(&Position) -> Vec<Turn>,
+    ) -> Self {
         Ruleset {
             generate_initial_position,
+            get_possible_moves
         }
     }
 
     pub fn generate_initial_position(&self) -> Position {
         (self.generate_initial_position)()
+    }
+
+    pub fn get_possible_moves(&self, position: &Position) -> Vec<Turn> {
+        (self.get_possible_moves)(position)
     }
 }
